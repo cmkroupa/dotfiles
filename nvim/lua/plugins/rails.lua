1bb5947461380327633aad241ca46e364b0926ce
Cameron Kroupa <ckroupa@uwo.ca>
2026-05-11 23:33:16 -0400

    add vim-rails which-key keymaps for ruby/eruby

--- nvim/lua/plugins/rails.lua
+++ nvim/lua/plugins/rails.lua
@@ -1,3 +1,22 @@
 return {
-	{ "tpope/vim-rails" },
+	{
+		"tpope/vim-rails",
+		ft = { "ruby", "eruby" },
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "ruby", "eruby" },
+				callback = function(args)
+					local map = vim.keymap.set
+					local opts = { buffer = args.buf }
+					local e = vim.tbl_extend
+					map("n", "<leader>ra", "<cmd>A<cr>",           e("force", opts, { desc = "Alternate" }))
+					map("n", "<leader>rr", "<cmd>R<cr>",           e("force", opts, { desc = "Related" }))
+					map("n", "<leader>rc", "<cmd>Econtroller<cr>", e("force", opts, { desc = "Controller" }))
+					map("n", "<leader>rm", "<cmd>Emodel<cr>",      e("force", opts, { desc = "Model" }))
+					map("n", "<leader>rv", "<cmd>Eview<cr>",       e("force", opts, { desc = "View" }))
+					map("n", "<leader>ri", "<cmd>Emigration<cr>",  e("force", opts, { desc = "Migration" }))
+				end,
+			})
+		end,
+	},
 }
