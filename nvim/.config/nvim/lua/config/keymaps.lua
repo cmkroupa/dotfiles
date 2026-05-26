5e793413e862ffcd4450765b5d7f251f57140f97
Cameron Kroupa <ckroupa@evertz.com>
2026-05-26 16:55:50 -0400

    unmap K

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 local map = vim.keymap.set
 
 map("n", "<Esc>", "<cmd>nohlsearch<cr>")
+map("n", "K", "<nop>")
 map("n", "<leader>l", "<cmd>Lazy<cr>",                       { desc = "Lazy" })
 map("n", "<leader>d", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics" })
 
